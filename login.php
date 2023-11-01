b7f711e32834a3dc119e438637024351d963c91a
LOLTH203 <131695470+LOLTH203@users.noreply.github.com>
2023-11-01 17:45:21 -0500

    Add files via upload
    Thingy

---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+    session_start();
+
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+        header("location: dashboard.html");
+        exit;
+    }
+    require_once "config.php";
+
+    $username = $password = "";
+    $username_err = $password_err = $login_err = "";
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(empty(trim($_POST["username"]))){
+            $username_err = "Please enter username.";
+        } else{
+            $username = trim($_POST["username"]);
+        }
+        if(empty(trim($_POST["password"]))){
+            $username_err = "Please enter password.";
+        } else{
+            $username = trim($_POST["password"]);
+        }
+        if(empty($username_err) && empty($password_err)){
+            // Prepare a select statement
+            $sql = "SELECT email, username, passwordHash FROM accounts WHERE username = ?";
+            if($stmt = mysqli_prepare($link, $sql)){
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "s", $param_username);
+                $param_username = $username;
+                if(mysqli_stmt_execute($stmt)){
+                    // Store result
+                    mysqli_stmt_store_result($stmt);
+                    
+                    // Check if username exists, if yes then verify password
+                    if(mysqli_stmt_num_rows($stmt) == 1){                    
+                        // Bind result variables
+                        mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                        if(mysqli_stmt_fetch($stmt)){
+                            if(password_verify($password, $hashed_password)){
+                                // Password is correct, so start a new session
+                                session_start();
+                                // Store data in session variables
+                                $_SESSION["loggedin"] = true;
+                                $_SESSION["id"] = $id;
+                                $_SESSION["username"] = $username;
+                                header("location: dashboard.php");
+                            } else{
+                                // Password is not valid, display a generic error message
+                                $login_err = "Invalid username or password.";
+                            }
+                        }
+                    } else{
+                        // Username doesn't exist, display a generic error message
+                        $login_err = "Invalid username or password.";
+                    }
+                } else{
+                    echo "Oopsy-woopsy! We had a fucky-wucky problem, pwease check back later.";
+                }
+            }
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+        // Close connection
+    mysqli_close($link);
+    }
+    
+    
+
+?>
